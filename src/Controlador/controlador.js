f668d21e35dc99568995d49a563f59f88f429f74
Alejandro Tapia Alvarez <a.tapia1908@gmail.com>
2021-09-08 00:24:01 -0600

    cambiar contraseña movido a controlador

--- src/Controlador/controlador.js
+++ src/Controlador/controlador.js
@@ -4,9 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const DAO_1 = __importDefault(require("./DAO"));
+const bcrypt = require('bcrypt');
 class Controlador {
     constructor() {
+        this.salts = 10;
         this.base_datos = DAO_1.default.get_instancia();
+    }
+    cambiar_contrasena(id_usuario, contrasena) {
+        return bcrypt.hash(contrasena, this.salts)
+            .then((hash) => {
+            return { resultado: this.base_datos.cambiar_contrasena(id_usuario, hash) };
+        });
     }
 }
 exports.default = Controlador;
