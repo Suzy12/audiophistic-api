1840d3985a1e2171867a5fe315b65e6f91aab877
Alejandro Tapia Alvarez <a.tapia1908@gmail.com>
2021-09-14 16:48:47 -0600

    Cambiado orden de envio de datos en regsitrar

--- src/Controlador/Gestor_Usuarios.ts
+++ src/Controlador/Gestor_Usuarios.ts
@@ -11,7 +11,7 @@ export default class Gestor_Usuarios {
 
     // Registra el usuario con los datos ingresados
     registrar_usuario(nombre: string, correo: string, contrasena: string): Promise<number> {
-        return this.base_datos.registrar_usuario(nombre, correo, contrasena)
+        return this.base_datos.registrar_usuario(correo, nombre, contrasena)
             .then((id_tipo: number) => {
                 return id_tipo;
             });
