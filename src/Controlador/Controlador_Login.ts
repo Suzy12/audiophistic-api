1d37d4fe955fc5fe8b1549d6520ff4f1858a85c8
Alejandro Tapia Alvarez <a.tapia1908@gmail.com>
2021-09-13 18:16:18 -0600

    Mejora identacion generacion_contrasena

--- src/Controlador/Controlador_Login.ts
+++ src/Controlador/Controlador_Login.ts
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 /* Se encarga de verificar el inicio de sesion y los
    Y los permisos que tiene un usuario para ciertas acciones */
-export default class Controlador_login { 
+export default class Controlador_login {
     // Definimos como hacer las llamadas la base de datos a traves del dao
     private base_datos: DAO;
     private manejador_token: Manejador_Tokens;
@@ -16,35 +16,36 @@ export default class Controlador_login {
 
     /* Verifica la combinación del correo con la contraseña
        Devuelve un token y el id del tipo del usuario en caso de ser correctos los datos*/
-    async verificar_contrasena(correo: string, contrasena: string): Promise<{id_tipo: number, token: string}> { //Metodo de verificacion de contrasena
+    async verificar_contrasena(correo: string, contrasena: string): Promise<{ id_tipo: number, token: string }> { //Metodo de verificacion de contrasena
         let datosUsuario: any = await this.base_datos.verificar_usuario(correo);
         let misma_contrasena = await bcrypt.compare(contrasena, datosUsuario.contrasena);
         console.log(datosUsuario);
         if (misma_contrasena) {
-            return { id_tipo:datosUsuario.id_tipo,
-                ...this.crear_token(datosUsuario.id_usuario,datosUsuario.email, datosUsuario.id_tipo)};
+            return {
+                id_tipo: datosUsuario.id_tipo,
+                ...this.crear_token(datosUsuario.id_usuario, datosUsuario.email, datosUsuario.id_tipo)
+            };
         } else {
             throw new Error("La contraseña es incorrecta");
         }
     }
 
     // Genera un token con los datos del usuario
-    crear_token(id_usuario: number, correo: string, id_tipo: number): {token: string} { //se llama al manejador de tokens
-        return this.manejador_token.crear_token(id_usuario,correo,id_tipo);
+    crear_token(id_usuario: number, correo: string, id_tipo: number): { token: string } { //se llama al manejador de tokens
+        return this.manejador_token.crear_token(id_usuario, correo, id_tipo);
     }
 
-    verificar_permisos(token: string, permiso: number): boolean{
+    verificar_permisos(token: string, permiso: number): boolean {
         return this.manejador_token.verificar_permisos(token, permiso) === permiso;
     }
 
     // Funcion para generar un string aleatorio para la recuperacion de contrasenias
-    generacion_password(): string{
+    generacion_password(): string {
+        var randomstring = Math.random() //genera un numero aleatorio
+            .toString(36) //lo comnvierte a base -36
+            .slice(-8); //corta los ultimos 8 caracteres
+        return randomstring;
 
-            var randomstring = Math.random() //genera un numero aleatorio
-                                                .toString(36) //lo comnvierte a base -36
-                                                            .slice(-8); //corta los ultimos 8 caracteres
-            return randomstring;
-    
-    } 
+    }
 
 }
